1fc828017e7188c6c25d7568008c0161f78a16ba
yusing <yusing@6uo.me>
2025-04-18 07:30:10 +0800

    refactor: remove opentelemetry stuff from api

--- client/hijack.go
+++ client/hijack.go
@@ -12,7 +12,6 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/versions"
 	"github.com/pkg/errors"
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 // postHijacked sends a POST request and hijacks the connection.
@@ -78,7 +77,7 @@ func setupHijackConn(dialer func(context.Context) (net.Conn, error), req *http.R
 	hc := &hijackedConn{conn, bufio.NewReader(conn)}
 
 	// Server hijacks the connection, error 'connection closed' expected
-	resp, err := otelhttp.NewTransport(hc).RoundTrip(req)
+	resp, err := hc.RoundTrip(req)
 	if err != nil {
 		return nil, "", err
 	}
