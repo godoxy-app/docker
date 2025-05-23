moby/moby

bridge: add option com.docker.network.bridge.trusted_host_interfaces
trusted_host_interface have access to published ports on container
addresses - enabling direct routing to the container via those
interfaces.

Signed-off-by: Rob Murray <rob.murray@docker.com>

libnetwork/drivers/bridge/internal/iptabler/endpoint.go
@@ -42,9 +42,21 @@ func (n *Network) modEndpoint(ctx context.Context, epIPv4, epIPv6 netip.Addr, en
 // Packets originating on the bridge's own interface and addressed directly to the
 // container are allowed - the host always has direct access to its own containers
 // (it doesn't need to use the port mapped to its own addresses, although it can).
+//
+// "Trusted interfaces" are treated in the same way as the bridge itself.
 func (n *Network) filterDirectAccess(ctx context.Context, ipv iptables.IPVersion, config NetworkConfigFam, epIP netip.Addr, enable bool) error {
 	if config.Unprotected || config.Routed || rawRulesDisabled(ctx) {
 		return nil
+	}
+	for _, ifName := range n.TrustedHostInterfaces {
+		accept := iptables.Rule{IPVer: ipv, Table: iptables.Raw, Chain: "PREROUTING", Args: []string{
+			"-d", epIP.String(),
+			"-i", ifName,
+			"-j", "ACCEPT",
+		}}
+		if err := appendOrDelChainRule(accept, "DIRECT ACCESS FILTERING - DROP", enable); err != nil {
+			return err
+		}
 	}
 	accept := iptables.Rule{IPVer: ipv, Table: iptables.Raw, Chain: "PREROUTING", Args: []string{
 		"-d", epIP.String(),

godoxy-app/docker

libnetwork/drivers/bridge/internal/iptabler/endpoint.go
@@ -43,6 +43,8 @@ func (n *Network) modEndpoint(ctx context.Context, epIPv4, epIPv6 netip.Addr, en
 // Packets originating on the bridge's own interface and addressed directly to the
 // container are allowed - the host always has direct access to its own containers
 // (it doesn't need to use the port mapped to its own addresses, although it can).
+//
+// "Trusted interfaces" are treated in the same way as the bridge itself.
 func (n *Network) filterDirectAccess(ctx context.Context, ipv iptables.IPVersion, config NetworkConfigFam, epIP netip.Addr, enable bool) error {
 	if n.Internal || config.Unprotected || config.Routed {
 		return nil
@@ -54,6 +56,16 @@ func (n *Network) filterDirectAccess(ctx context.Context, ipv iptables.IPVersion
 	// live-restore restart will take effect.
 	if rawRulesDisabled(ctx) {
 		enable = false
+	}
+	for _, ifName := range n.TrustedHostInterfaces {
+		accept := iptables.Rule{IPVer: ipv, Table: iptables.Raw, Chain: "PREROUTING", Args: []string{
+			"-d", epIP.String(),
+			"-i", ifName,
+			"-j", "ACCEPT",
+		}}
+		if err := appendOrDelChainRule(accept, "DIRECT ACCESS FILTERING - ACCEPT", enable); err != nil {
+			return err
+		}
 	}
 	accept := iptables.Rule{IPVer: ipv, Table: iptables.Raw, Chain: "PREROUTING", Args: []string{
 		"-d", epIP.String(),