moby/moby

c8d/delete: Support deleting specific platforms
This change adds the ability to delete a specific platform from a
multi-platform image.

Previously, image deletion was an all-or-nothing operation - when
deleting a multi-platform image, all platforms would be removed
together. This change allows users to selectively remove individual
platforms from a multi-architecture image while keeping other platforms
intact.

Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

api/server/router/image/image_routes.go
@@ -323,9 +323,19 @@ func (ir *imageRouter) deleteImages(ctx context.Context, w http.ResponseWriter, 
 	force := httputils.BoolValue(r, "force")
 	prune := !httputils.BoolValue(r, "noprune")
 
+	var platforms []ocispec.Platform
+	if versions.GreaterThanOrEqualTo(httputils.VersionFromContext(ctx), "1.50") {
+		p, err := httputils.DecodePlatforms(r.Form["platform"])
+		if err != nil {
+			return err
+		}
+		platforms = p
+	}
+
 	list, err := ir.backend.ImageDelete(ctx, name, imagetypes.RemoveOptions{
 		Force:         force,
 		PruneChildren: prune,
+		Platforms:     platforms,
 	})
 	if err != nil {
 		return err

client/image_remove.go
@@ -19,6 +19,14 @@ func (cli *Client) ImageRemove(ctx context.Context, imageID string, options imag
 		query.Set("noprune", "1")
 	}
 
+	if len(options.Platforms) > 0 {
+		p, err := encodePlatforms(options.Platforms...)
+		if err != nil {
+			return nil, err
+		}
+		query["platform"] = p
+	}
+
 	resp, err := cli.delete(ctx, "/images/"+imageID, query, nil)
 	defer ensureReaderClosed(resp)
 	if err != nil {

client/image_remove_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/errdefs"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"gotest.tools/v3/assert"
 	is "gotest.tools/v3/assert/cmp"
 )
@@ -40,6 +41,7 @@ func TestImageRemove(t *testing.T) {
 	removeCases := []struct {
 		force               bool
 		pruneChildren       bool
+		platform            *ocispec.Platform
 		expectedQueryParams map[string]string
 	}{
 		{
@@ -49,12 +51,22 @@ func TestImageRemove(t *testing.T) {
 				"force":   "",
 				"noprune": "1",
 			},
-		}, {
+		},
+		{
 			force:         true,
 			pruneChildren: true,
 			expectedQueryParams: map[string]string{
 				"force":   "1",
 				"noprune": "",
+			},
+		},
+		{
+			platform: &ocispec.Platform{
+				Architecture: "amd64",
+				OS:           "linux",
+			},
+			expectedQueryParams: map[string]string{
+				"platform": `{"architecture":"amd64","os":"linux"}`,
 			},
 		},
 	}
@@ -92,10 +104,15 @@ func TestImageRemove(t *testing.T) {
 				}, nil
 			}),
 		}
-		imageDeletes, err := client.ImageRemove(context.Background(), "image_id", image.RemoveOptions{
+
+		opts := image.RemoveOptions{
 			Force:         removeCase.force,
 			PruneChildren: removeCase.pruneChildren,
-		})
+		}
+		if removeCase.platform != nil {
+			opts.Platforms = []ocispec.Platform{*removeCase.platform}
+		}
+		imageDeletes, err := client.ImageRemove(context.Background(), "image_id", opts)
 		if err != nil {
 			t.Fatal(err)
 		}

godoxy-app/docker

api/server/router/image/image_routes.go
@@ -326,9 +326,19 @@ func (ir *imageRouter) deleteImages(ctx context.Context, w http.ResponseWriter, 
 	force := httputils.BoolValue(r, "force")
 	prune := !httputils.BoolValue(r, "noprune")
 
+	var platforms []ocispec.Platform
+	if versions.GreaterThanOrEqualTo(httputils.VersionFromContext(ctx), "1.50") {
+		p, err := httputils.DecodePlatforms(r.Form["platforms"])
+		if err != nil {
+			return err
+		}
+		platforms = p
+	}
+
 	list, err := ir.backend.ImageDelete(ctx, name, imagetypes.RemoveOptions{
 		Force:         force,
 		PruneChildren: prune,
+		Platforms:     platforms,
 	})
 	if err != nil {
 		return err

client/image_remove.go
@@ -19,6 +19,14 @@ func (cli *Client) ImageRemove(ctx context.Context, imageID string, options imag
 		query.Set("noprune", "1")
 	}
 
+	if len(options.Platforms) > 0 {
+		p, err := encodePlatforms(options.Platforms...)
+		if err != nil {
+			return nil, err
+		}
+		query["platforms"] = p
+	}
+
 	resp, err := cli.delete(ctx, "/images/"+imageID, query, nil)
 	defer ensureReaderClosed(resp)
 	if err != nil {

client/image_remove_test.go
@@ -12,6 +12,7 @@ import (
 
 	cerrdefs "github.com/containerd/errdefs"
 	"github.com/docker/docker/api/types/image"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"gotest.tools/v3/assert"
 	is "gotest.tools/v3/assert/cmp"
 )
@@ -40,6 +41,7 @@ func TestImageRemove(t *testing.T) {
 	removeCases := []struct {
 		force               bool
 		pruneChildren       bool
+		platform            *ocispec.Platform
 		expectedQueryParams map[string]string
 	}{
 		{
@@ -49,12 +51,22 @@ func TestImageRemove(t *testing.T) {
 				"force":   "",
 				"noprune": "1",
 			},
-		}, {
+		},
+		{
 			force:         true,
 			pruneChildren: true,
 			expectedQueryParams: map[string]string{
 				"force":   "1",
 				"noprune": "",
+			},
+		},
+		{
+			platform: &ocispec.Platform{
+				Architecture: "amd64",
+				OS:           "linux",
+			},
+			expectedQueryParams: map[string]string{
+				"platforms": `{"architecture":"amd64","os":"linux"}`,
 			},
 		},
 	}
@@ -92,10 +104,16 @@ func TestImageRemove(t *testing.T) {
 				}, nil
 			}),
 		}
-		imageDeletes, err := client.ImageRemove(context.Background(), "image_id", image.RemoveOptions{
+
+		opts := image.RemoveOptions{
 			Force:         removeCase.force,
 			PruneChildren: removeCase.pruneChildren,
-		})
+		}
+		if removeCase.platform != nil {
+			opts.Platforms = []ocispec.Platform{*removeCase.platform}
+		}
+
+		imageDeletes, err := client.ImageRemove(context.Background(), "image_id", opts)
 		assert.NilError(t, err)
 		assert.Check(t, is.Len(imageDeletes, 2))
 	}